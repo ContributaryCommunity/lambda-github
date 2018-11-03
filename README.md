56464ed1b0ad505a7305ebc3ebd13d3a3041850d
Owen Buckley <owenbuckley13@gmail.com>
2018-11-03 18:43:08 -0400

    Enhancement/create lambda functions (#1)
    * got issue request working locally
    
    * lint
    
    * ci
    
    * working locally
    
    * working in AWS
    
    * repositories lambda
    
    * docs
    
    * github templates
    
    * update to reflect proxy integration
    
    * fix linting
    
    * clean up error handling

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # lambda-github
-Lambda for (reverse) proxying GitHub API requests from the frontend
+[![GitHub release](https://img.shields.io/github/tag/ContributaryCommunity/lambda-topology.svg)](https://github.com/ContributaryCommunity/lambda-topology/tags)
+![CircleCI branch](https://img.shields.io/circleci/project/github/ContributaryCommunity/lambda-topology/master.svg?style=plastic)
+[![GitHub issues](https://img.shields.io/github/issues-raw/ContributaryCommunity/lambda-topology.svg)](https://github.com/ContributaryCommunity/lambda-topology/issues)
+[![GitHub issues](https://img.shields.io/github/issues-pr-raw/ContributaryCommunity/lambda-topology.svg)](https://github.com/ContributaryCommunity/lambda-topology/issues)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/ContributaryCommunity/lambda-topology/master/LICENSE.md)
+
+## Overview
+Lambda for (reverse) proxying GitHub API requests from the frontend for getting live repositories and issues data.
+
+## Workflow
+Changes submitted to _src/*.js_ are uploaded manually to the AWS Lambda function console.  Currently there are two Lambdas used
+- _issues_ - Fetches issues from the GitHub API for a given project name and repository name
+- _repositories_ - Fetches repositories from the GitHub API for a given project name and repository type (_org_ or _user_)
+
+## Development
+
+For contributing to this project and testing the output locally, you will need
+1. [NodeJS](https://nodejs.org/) 8.x
+1. [Yarn](https://yarnpkg.com) 1.x
+1. Create _tmp/_ directory in the root of the project
+1. Set envrionment variable for devleopment: `export NODE_ENV=development`
+1. Export a [GitHub personal access token](https://blog.github.com/2013-05-16-personal-api-tokens/): `export ACCESS_TOKEN=XXX`
+
+- `yarn lint` - Validatse all JS and JSON passes linting
+- `yarn function:issues` - Run _src/issues.js_ and by default output the file in _tmp/_
+- `yarn function:repositories` - Run _src/repositories.js_ and by default output the file in _tmp/_
+
+## Release Procedure
+1. Merge all changes into master
+1. Bump _package.json_, `git tag` and push everything to master
+1. Upload the contents of _src/index.js_ to Lambda
+1. Create [Release Notes](https://github.com/ContributaryCommunity/lambda-topology/releases) in GitHub
---
+++ src/issues.js
@@ -0,0 +1,102 @@
+/*
+ * This script assumes permissions a GitHub access token available as an environment variable
+ */
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+const host = 'api.github.com';
+const isProduction = process.env.NODE_ENV === 'production';
+const isCI = process.env.NODE_ENV === 'ci';
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const outputFile = 'issues.json';
+const tmpDir = path.join(__dirname, '..', 'tmp');
+const headers = {
+  'Accept': 'application/vnd.github.v3+json',
+  'Authorization': `token ${ACCESS_TOKEN}`,
+  'User-Agent': 'Contributary.community' // https://developer.github.com/v3/#user-agent-required
+};
+
+// expose handler for Lambda
+exports.run = run;
+
+if (!isProduction) {
+  const mockEvent = {
+    queryStringParameters: {
+      projectName: 'contributarycommunity',
+      repoName: 'www.contributary.community' 
+    }
+  };
+
+  run(mockEvent);
+}
+
+function writeToFilesystem(response) {
+  const filePath = `${tmpDir}/${outputFile}`;
+
+  fs.writeFileSync(`${filePath}`, JSON.stringify(response, null, 2), (err) => {
+    if (err) {
+      return console.error(err); // eslint-disable-line no-console
+    }
+    
+    console.log(`File ${filePath} was saved!`); // eslint-disable-line no-console
+  });
+}
+
+function handleIssuesResponse(response) {
+  console.log('handleIssuesResponse.isProduction', isProduction); // eslint-disable-line no-console
+
+  if (isProduction) {
+    return {
+      statusCode: 200,
+      headers: {},
+      body: JSON.stringify(response),
+      isBase64Encoded: false
+    };
+  } else if (isCI) {
+    return {};
+  } else {
+    writeToFilesystem(response);
+  }
+}
+
+// https://developer.github.com/v3/issues/
+// application/vnd.github.symmetra-preview+json
+function getIssues(projectName, repositoryName) {
+  const midFix = `${projectName}/${repositoryName}`;
+  const options = {
+    host,
+    path: `/repos/${midFix}/issues`,
+    headers
+  };
+
+  console.log(`GET issues for ${midFix}`); // eslint-disable-line no-console
+  console.log('GET options', options); // eslint-disable-line no-console
+
+  return new Promise((resolve, reject) => {
+    https.get(options, (resp) => {
+      let data = '';
+
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      resp.on('end', () => {
+        resolve(JSON.parse(data));
+      });
+
+    }).on('error', (err) => {
+      reject(err);
+    });
+  });
+}
+
+function run(event = {}) {
+  const { projectName, repoName } = event.queryStringParameters;
+
+  return getIssues(projectName, repoName)
+    .then(handleIssuesResponse)
+    .catch((error) => {
+      console.error(error); // eslint-disable-line no-console
+    });
+
+}
